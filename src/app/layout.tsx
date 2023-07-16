6ddd50726a5dc5775155f2278b95020bac2f9422
BrandonWu500 <brwu17@gmail.com>
2023-07-16 09:23:02 -0400

    feat: remove inter font
    because mui uses roboto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 
 import ThemeRegistry from '@/theme/ThemeRegistry';
 
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -20,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ThemeRegistry>
-        <body className={inter.className}>{children}</body>
+        <body>{children}</body>
       </ThemeRegistry>
     </html>
   );
