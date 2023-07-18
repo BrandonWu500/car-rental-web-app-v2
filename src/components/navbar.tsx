268c74565387c4a027b564c31e983ce950919915
BrandonWu500 <brwu17@gmail.com>
2023-07-18 08:44:53 -0400

    feat: add logo to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { AppBar, Container, Toolbar } from '@mui/material';
+import { Logo } from './ui/logo';
 
 export const Navbar = () => {
   return (
     <AppBar position="sticky">
       <Container maxWidth="xl">
-        <Toolbar></Toolbar>
+        <Toolbar>
+          <Logo />
+        </Toolbar>
       </Container>
     </AppBar>
   );
