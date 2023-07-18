b4f60abc0cd66eadee982830b74bbe8667a582d5
BrandonWu500 <brwu17@gmail.com>
2023-07-18 16:51:57 -0400

    feat: add search component to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,22 @@
 'use client';
 
 import { AppBar, Container, Toolbar } from '@mui/material';
+import { Search } from './search';
 import { Logo } from './ui/logo';
 
 export const Navbar = () => {
   return (
-    <AppBar position="sticky">
+    <AppBar position="sticky" sx={{ backgroundColor: 'white' }}>
       <Container maxWidth="xl">
-        <Toolbar>
+        <Toolbar
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            padding: 2,
+          }}
+        >
           <Logo />
+          <Search />
         </Toolbar>
       </Container>
     </AppBar>
